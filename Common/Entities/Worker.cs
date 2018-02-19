04c4a8a060293d148c98f21b2381e7898105d510
udi101 <udimaz@gmail.com>
2018-02-19 22:59:21 +0200

    Using Dapper

--- Common/Entities/Worker.cs
+++ Common/Entities/Worker.cs
@@ -3,7 +3,8 @@
     public class Worker
     {
         public string Name { get; set; }
-        public string _MyProperty { get; set; }
+        public string LastName { get; set; }
         public int Age { get; set; }
+        public int Salary { get; set; }
     }
 }
--- Reactive.DAL.Main/WorkersDB.cs
+++ Reactive.DAL.Main/WorkersDB.cs
@@ -3,7 +3,9 @@ using System.Data.SqlClient;
 using System.Data;
 using System.Configuration;
 using Common.Entities;
+using Dapper;
 using System;
+using System.Linq;
 
 namespace Reactive.DAL.Main
 {
@@ -21,20 +23,23 @@ namespace Reactive.DAL.Main
             {
                 //return await conn.GetAsync<Workers>();
 
-                using (SqlCommand Command = new SqlCommand("select * from dbo.Workers", Conn))
-                {
-                    Conn.Open();
-                    SqlDataReader Reader = Command.ExecuteReader();
-                    while (Reader.Read())
-                    {
-                        Result.Add(new Worker() {
-                            Name = Reader["name"].ToString(),
-                            Age = Convert.ToInt16(Reader["age"]),
-                            _MyProperty = Reader["lastName"].ToString()
-                        });
-                    }
-                    Reader.Close();
-                }
+                Conn.Open();
+                return Conn.Query<Worker>("select * from dbo.Workers").ToList();
+
+                //using (SqlCommand Command = new SqlCommand("select * from dbo.Workers", Conn))
+                //{
+                //    Conn.Open();
+                //    SqlDataReader Reader = Command.ExecuteReader();
+                //    while (Reader.Read())
+                //    {
+                //        Result.Add(new Worker() {
+                //            Name = Reader["name"].ToString(),
+                //            Age = Convert.ToInt16(Reader["age"]),
+                //            _MyProperty = Reader["lastName"].ToString()
+                //        });
+                //    }
+                //    Reader.Close();
+                //}
             }
             return Result;
         }
